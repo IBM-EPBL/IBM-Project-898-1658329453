e4da47968740431b440d928e0b23c176308f1df4
RArunpandiyan <113301163+RArunpandiyan@users.noreply.github.com>
2022-11-17 11:04:33 +0530

    Add files via upload

---
+++ Assignments/Arunpandiyan R(Lead 3)/Assignment 1/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template 
+app = Flask(__name__ )
+@app.route('/', methods =["GET", "POST"]) 
+def Index():
+ return render_template('index.html') 
+@app.route('/Blog') 
+def Blog():
+ return render_template('blog.html') 
+@app.route('/Signup')
+def Signup():
+ return render_template('signup.html') 
+@app.route('/Signin')
+def Signin():
+ return render_template('signin.html') 
+if __name__ =='__main__':
+ app.debug = True
+ app.run()
